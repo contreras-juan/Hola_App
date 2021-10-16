7fe77ed4478fdecbc9f59eb9c45e094522b33abc
Juan <j.contrerasa94@gmail.com>
2021-10-16 15:24:46 -0500

    Linked frontend with domain and persistence

--- HolaWeb.App/HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
+++ HolaWeb.App/HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
@@ -2,21 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HolaWeb.App.Persistencia.AppRepositorios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HolaWeb.App.Dominio;
 
 namespace HolaWeb.App.Frontend.Pages
 {
     public class ListModel : PageModel
     {
-        private string [] saludos = {"Buenos días", "Buenas tardes", "Buenas noches", "Hasta mañana"};
+        // private string [] saludos = {"Buenos días", "Buenas tardes", "Buenas noches", "Hasta mañana"};
         
-        public List<string> ListaSaludos {get;set;}
+        // public List<string> ListaSaludos {get;set;}
+
+        private readonly IRepositoriosSaludos repositoriossaludos;
+        public IEnumerable<Saludo> Saludos {get;set;}
+
+        public ListModel(IRepositoriosSaludos repositoriossaludos)
+        {
+            this.repositoriossaludos=repositoriossaludos;
+        }
 
         public void OnGet()
         {
-            ListaSaludos = new List<string>();
-            ListaSaludos.AddRange(saludos);
+            // ListaSaludos = new List<string>();
+            // ListaSaludos.AddRange(saludos);
+            Saludos = repositoriossaludos.GetAll();
         }
     }
 }
